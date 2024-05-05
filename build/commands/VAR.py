f9a187afcfb97088aec5638c450c219d8dcb66f0
Stanley <stano5340@gmail.com>
2024-05-05 12:23:21 +0200

    Update a1.0.3 Remade escaping Added saving command's return into variables using the save to/as/in command Syntax: save COMMAND to/as/in variable_name

--- build/commands/VAR.py
+++ build/commands/VAR.py
@@ -4,19 +4,19 @@ def declare(x, vars):
     if len(x) < 4: raise SyntaxError("To declare a variable follow this syntax: 'var name = value'. Don't forget the spaces!")
     elif x[2] != "=": raise SyntaxError("To declare a variable follow this syntax: 'var name = value'. Don't forget the spaces!")
     elif not re.match(r'\w+', x[1]): raise ValueError("Variable name must only contain letters, numbers and underscores.")
-    else: vars[x[1]] = " ".join(x[3:])
+    else: vars[x[1]] = " ".join(x[3:]); return None, None
 
 
 
 def pr(x, vars, fn):
     if len(x) < 2: raise SyntaxError("Please provide a value to print.")
-    else: print(fn(" ".join(x[1:])))
+    else: return(fn(" ".join(x[1:]))), None
 
 
 
 def delete(x, vars):
     if len(x) == 2:
-        if x[1] in vars: del vars[x[1]]
+        if x[1] in vars: del vars[x[1]]; return None, None
         else: raise ValueError("This variable doesn't exist.")
     else: raise SyntaxError("To delete a variable follow this syntax: 'del variable_name'")
    
@@ -24,8 +24,10 @@ def delete(x, vars):
 
 def listall(x, vars):
     if len(x) == 1:
+        s = "\n"
         for k,v in vars.items():
-            print(k, "=", v)
+            s+=k+" = "+v+"\n"
         if len(vars.items()) == 0:
-            print("There are no saved variables.")
+            s = "There are no saved variables."
+        return s, s
     else: raise SyntaxError("Unexpected parameter provided.")
--- build/fn.py
+++ build/fn.py
@@ -2,6 +2,8 @@ from .commands import *
 import re
 
 
+REPLACINGS = [("\\n", "\n"), ("\\s", " "), ("\\t", "\t"), ("\\$lc;", "{"), ("\\$rc;", "}"), ("\\bs;", "\\")]
+
 def execute(cmd, vars):
     
     def replacevars(m):
@@ -9,19 +11,36 @@ def execute(cmd, vars):
         return vars.get(key, m.group(0))
     
     def replaceescapes(m):
-        m = m.replace("\\n", "\n").replace("\\s", " ").replace("\\t","\t").replace("\\0", "").replace("\\$lc;", "{").replace("\\$rc;", "}").replace("\\$bs;", "\\")
+        for i in REPLACINGS:
+            m = m.replace(i[0], i[1])
+        m = m.replace("\\0","")
         return m
 
     x = cmd.strip()
     x = re.sub(r'\{(\w+)\}', replacevars, x)
     x = x.split()
     x[0] = x[0].lower()
+    P, R = None, None
     
     match x[0]:
-        case "github": GITHUB.github(x, vars)
-        case "var": VAR.declare(x, vars)
-        case "print": VAR.pr(x, vars, replaceescapes)
-        case "del": VAR.delete(x, vars)
-        case "variables": VAR.listall(x, vars)
-        case "web": WEB.web(x, vars)
+        case "github": P,R = GITHUB.github(x, vars)
+        case "var": P,R = VAR.declare(x, vars)
+        case "print": P,R = VAR.pr(x, vars, replaceescapes)
+        case "del": P,R = VAR.delete(x, vars)
+        case "variables": P,R = VAR.listall(x, vars)
+        case "web": P,R = WEB.web(x, vars)
+        case "save": savetovariable(x, vars)
         case _: raise SyntaxError("This command doesn't exist.")
+    
+    if P != None: print(P)
+    if type(R) == type("str"):
+        for i in REPLACINGS[::-1]:
+            R = R.replace(i[1], i[0])
+    return R
+
+def savetovariable(x, vars):
+    if len(x) < 4: raise SyntaxError("The save commands needs to follow this syntax: 'save COMMAND to/as/in variable_name'")
+    else:
+        if x[-2] not in {"to", "as", "in"}: raise SyntaxError("The save commands needs to follow this syntax: 'save COMMAND to/as/in variable_name'")
+        else:
+            vars[x[-1]] = execute(" ".join(x[1:-2]), vars)
